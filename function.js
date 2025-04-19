8934cd7f90abc904299b97a905cc13f90ad31c19
Nasaipin <130257385+Nasaipin@users.noreply.github.com>
2025-04-19 02:09:36 +0000

    Add files via upload

---
+++ function.js
@@ -0,0 +1,7 @@
+const navbarToggle = document.getElementById('navbar-toggle');
+const navbarNav = document.querySelector('.navbar-nav');
+
+navbarToggle.addEventListener('click', () =>{
+    // navbarNav.classList.toggle('show');
+    alert ("toggle got cliked");
+});
